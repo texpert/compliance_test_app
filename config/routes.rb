ddae006c8edf849d9feb7e8cd7dfe9116900d761
texpert <branzeanu.aurel@gmail.com>
2026-04-13 21:52:21 +0300

    feat(routes): add AIS endpoints for manual accounts and transactions

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,15 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get  'ais/consents',     to: 'ais_consents#index',  as: :ais_consents_list
+  post 'ais/consents',     to: 'ais_consents#create', as: :ais_consents
+  get  'ais/consents/:id', to: 'ais_consents#show',   as: :ais_consent
+  # RESTful endpoints for accounts and transactions associated with a consent
+  get 'ais/consents/:id/accounts', to: 'ais_accounts#index', as: :ais_consent_accounts
+  # Also allow fetching transactions for a consent without specifying account_id
+  get 'ais/consents/:id/accounts/transactions', to: 'ais_transactions#index', as: :ais_consent_transactions
+  get 'ais/consents/:id/accounts/:account_id/transactions', to: 'ais_transactions#index', as: :ais_consent_account_transactions
+  get 'callback/:id', to: 'ais_callbacks#show', as: :ais_callback
+
+  root 'ais_consents#index'
 end
